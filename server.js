84a7ab14a9a4270ecf2b2583bf79032e8a030fa1
Nicholas Carr <nicholas.carr@rogers.com>
2016-10-22 13:21:53 -0400

    Fixed deprecated code
    Added yet another dependency

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var restclient = require('node-rest-client').Client;
 restclient = new restclient();
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 app.listen(80, function(){
   console.log('listening on 80');
 });
@@ -14,4 +18,3 @@ app.get('/', function(req, res) {
 app.post('/where', function(req, res) {
   var location = req.body.where;
 });
-app.use(express.static(__dirname + '/public'));
